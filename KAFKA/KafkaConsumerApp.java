977afe754457c9dbcc5b1a52c405e1d3834b24a6
syshub-rgb <sidneisilvaamaral@gmail.com>
2021-06-02 08:34:31 -0300

    Projeto Gravado com Sucesso

--- KAFKA/KafkaConsumerApp.java
+++ KAFKA/KafkaConsumerApp.java
@@ -1,7 +1,3 @@
-// Esse projeto é Academico - Realizado nos cursos da Data Sciencie Academy
-// https://www.datascienceacademy.com.br/
-
-
 import org.apache.kafka.common.*;
 import org.apache.kafka.clients.consumer.*;
 
@@ -13,13 +9,24 @@ public class KafkaConsumerApp {
         // Cria a classe Properties para instanciar o Consumer com as configurações desejadas:
         Properties props = new Properties();
 
-        
+        props.put("bootstrap.servers", "ec2-52-15-70-20.us-east-2.compute.amazonaws.com:9092, ec2-52-15-70-20.us-east-2.compute.amazonaws.com:9093");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+
         props.put("fetch.min.bytes", 1);
         props.put("group.id", "");
         props.put("heartbeat.interval.ms", 3000);
         props.put("max.partition.fetch.bytes", 1048576);
         props.put("session.timeout.ms", 30000);
-
+        props.put("auto.offset.reset", "latest");
+        props.put("connections.max.idle.ms", 540000);
+        props.put("enable.auto.commit", true);
+        props.put("exclude.internal.topics", true);
+        props.put("max.poll.records", 2147483647);
+        props.put("partition.assignment.strategy", "org.apache.kafka.clients.consumer.RangeAssignor");
+        props.put("request.timeout.ms", 40000);
+        props.put("auto.commit.interval.ms", 5000);
+        props.put("fetch.max.wait.ms", 500);
         props.put("metadata.max.age.ms", 300000);
         props.put("reconnect.backoff.ms", 50);
         props.put("retry.backoff.ms", 100);
@@ -35,5 +42,45 @@ public class KafkaConsumerApp {
         partitions.add(new TopicPartition("topico5", 1));
         partitions.add(new TopicPartition("topico5", 2));
 
- 
+        // Atribui partições ao consumer:
+        myConsumer.assign(partitions);
+
+        // Recupera a lista de assinaturas do tópico do objeto interno SubscriptionState:
+        Set<TopicPartition> assignedPartitions = myConsumer.assignment();
+
+        // Imprima as atribuições da partição:
+        printSet(assignedPartitions);
+
+        // Comece a pesquisar por mensagens:
+        try {
+            while (true){
+                ConsumerRecords records = myConsumer.poll(10);
+                printRecords(records);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            myConsumer.close();
+        }
+
+    }
+
+    private static void printSet(Set<TopicPartition> collection){
+        if (collection.isEmpty()) {
+            System.out.println("Eu não tenho nenhuma partição atribuída ainda...");
+        }
+        else {
+            System.out.println("Eu estou designado a seguir partições:");
+            for (TopicPartition partition: collection){
+                System.out.println(String.format("Partição: %s no Tópico: %s", Integer.toString(partition.partition()), partition.topic()));
+            }
+        }
+    }
+
+    private static void printRecords(ConsumerRecords<String, String> records)
+    {
+        for (ConsumerRecord<String, String> record : records) {
+            System.out.println(String.format("Tópico: %s, Partição: %d, Offset: %d, Key: %s, Value: %s", record.topic(), record.partition(), record.offset(), record.key(), record.value()));
+        }
+    }
 }
--- KAFKA/KafkaProducerApp.java
+++ KAFKA/KafkaProducerApp.java
@@ -1,6 +1,3 @@
-// Esse projeto é Academico - Realizado nos cursos da Data Sciencie Academy
-// https://www.datascienceacademy.com.br/
-
 import org.apache.kafka.clients.producer.*;
 
 import java.text.*;
@@ -13,6 +10,10 @@ public class KafkaProducerApp {
         // Cria a classe Properties para instanciar o Producer com as configurações desejadas:
         Properties props = new Properties();
 
+        props.put("bootstrap.servers", "ec2-52-15-70-20.us-east-2.compute.amazonaws.com:9092, ec2-52-15-70-20.us-east-2.compute.amazonaws.com:9093");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
         props.put("acks", "1");
         props.put("buffer.memory", 33554432);
         props.put("compression.type", "none");
@@ -21,6 +22,10 @@ public class KafkaProducerApp {
         props.put("client.id", "");
         props.put("linger.ms", 0);
         props.put("max.block.ms", 60000);
+        props.put("max.request.size", 1048576);
+        props.put("partitioner.class", "org.apache.kafka.clients.producer.internals.DefaultPartitioner");
+        props.put("request.timeout.ms", 30000);
+        props.put("max.in.flight.requests.per.connection", 5);
         props.put("retry.backoff.ms", 5);
 
 
@@ -32,6 +37,19 @@ public class KafkaProducerApp {
         // Número de registros para enviar
         int numberOfRecords = 1000;
 
+        // Quanto tempo você deseja aguardar antes que o próximo registro seja enviado
+        long sleepTimer = 0;
 
+        try {
+            for (int i = 0; i < numberOfRecords; i++ )
+                myProducer.send(new ProducerRecord<String, String>(topic, String.format("Mensagem de Teste: %s  enviada em %s", Integer.toString(i), dtFormat.format(new Date()))));
+            Thread.sleep(sleepTimer);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            myProducer.close();
+        }
+
+    }
 }
 
